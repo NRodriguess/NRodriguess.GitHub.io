5b83337cdda80cb4f9cac0d4b41d978a74df2c64
NRodriguess <nickrodrigues006@gmail.com>
2021-05-01 16:34:34 -0300

    updated

--- WDD230/lesson1/currentDate.js
+++
@@ -1,3 +0,0 @@
-
-     const options = {weekday: 'long', day: 'numeric' , month: 'long' , year: 'numeric'};
-     document.getElementById("currentDay").textContent = new Date().toLocaleDateString('en-US', options);
