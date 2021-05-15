297f9840b0f9032a89b3cdc41b1a4af0ef9549a1
NRodriguess <nickrodrigues006@gmail.com>
2021-05-15 15:03:31 -0300

    lesson4

---
+++ WDD230/lesson4/js/preston.js
@@ -0,0 +1,3 @@
+const options = {weekday: 'long', day: 'numeric' , month: 'long' , year: 'numeric'};
+     document.getElementById("currentDay").textContent = new Date().toLocaleDateString('en-US', options);
+     document.getElementById("currentyear").textContent = Date.getFullYear;
