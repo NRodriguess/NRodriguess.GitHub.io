76c50dd9a7993e912eab43e33157cc1f8cce3bf3
NRodriguess <nickrodrigues006@gmail.com>
2021-05-27 08:22:28 -0300

    lesson5

--- WDD230/lesson5/js/banner.js
+++ WDD230/lesson5/js/banner.js
@@ -1,17 +1,5 @@
-function check() {
-  var now = new Date();
-  var month = now.getMonth();
-  var dayOfMonth = now.getDate();
-  var dayOfWeek = now.getDay();
-  let output = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
 
-  if (dayOfWeek == 3 ) {   //0 is Sunday, 6 is Saturday
-   output = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion.";
-
-} else {
-     
-  }
-  
-  document.getElementById("output").innerHTML = output;
-}
-
+let thedate = new Date();
+let weekDay = thedate.getDate();
+if (thedate.getDay() == 4 ) { 
+  document.querySelector("aside").style.display = 'block';}
