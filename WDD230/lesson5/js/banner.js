92ce838844db82d92ac8cf0b9bb975dc66b8ac80
NRodriguess <nickrodrigues006@gmail.com>
2021-05-27 21:28:05 -0300

    lesson5

--- WDD230/lesson5/js/banner.js
+++ WDD230/lesson5/js/banner.js
@@ -1,5 +1,5 @@
 
 let thedate = new Date();
 let weekDay = thedate.getDate();
-if (thedate.getDay() == 4 ) { 
-  document.querySelector("aside").style.display = 'block';}
+if (thedate.getDay() == 6 ) { 
+  document.querySelector("aside")}
