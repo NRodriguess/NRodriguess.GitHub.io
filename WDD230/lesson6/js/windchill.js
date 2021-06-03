4a289730b4386e95972ea044089f4ef131288d61
NRodriguess <nickrodrigues006@gmail.com>
2021-06-03 11:38:43 -0300

    lesson6

--- WDD230/lesson6/js/windchill.js
+++ WDD230/lesson6/js/windchill.js
@@ -1,10 +1,16 @@
-let temp = document.querySelector("#temp")
-let speed = document.querySelector("#windspeed")
+let temp = parseFloat(document.querySelector("#temp").textContent);
+let speed = parseFloat(document.querySelector("#windspeed").textContent);
 
-if()
+if(temp <= 50 && speed > 3.0){
+  let result = windChill(temp, speed);
+  document.querySelector('#windoutput').textContent = result.toFixed(2);
+}
+else {
+  document.querySelector('#windoutput').textContent = "N/A";
+}
 
 
 function windChill(t, s) {
-  let  = 35.74 + (0.6215*t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s,0.16))
-  return windFactor;
+  let windChill = 35.74 + (0.6215*t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s,0.16))
+  return windChill;
 }
