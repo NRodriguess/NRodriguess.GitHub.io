9a9818caabdb77cfdd9841ad9666c4233e821cbc
NRodriguess <nickrodrigues006@gmail.com>
2021-06-18 22:18:15 -0300

    lesson9

--- WDD230/lesson9/js/prophets.js
+++ WDD230/lesson9/js/prophets.js
@@ -12,12 +12,24 @@ fetch(requestURL)
     for (let i = 0; i < prophets.length; i++ ) {
       let card = document.createElement('section');
       let h2 = document.createElement('h2');
+      let img = document.createElement('img')
+      let h4 = document.createElement('p')
+      let p = document.createElement('p')
 
-      h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-      image.setAttribute('src', prophets[i].imageurl);
+      h2.innerHTML =  `${prophets[i].name} <span class="purple">${prophets[i].lastname}</span>`;
+      h4.innerHTML = 'Date of Birth - '  + prophets[i].birthdate;
+      p.innerHTML = 'Date of Place - '  + prophets[i].birthplace;
+      img.setAttribute('src', prophets[i].imageurl);
+      img.setAttribute('alt', `The official portrait of ${prophets[i].name} ${prophets[i].lastname}!`);
+      
 
 
       card.append(h2);
+      card.append(h4);
+      card.append(p);
+      card.append(img);
+
+
 
       document.querySelector('div.cards').append(card);
 
