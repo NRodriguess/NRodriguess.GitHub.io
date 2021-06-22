ee292a5f035e76d92b162ce34f8800be2a313029
NRodriguess <nickrodrigues006@gmail.com>
2021-06-22 20:44:40 -0300

    lesson9

--- WDD230/lesson9/js/town.js
+++ WDD230/lesson9/js/town.js
@@ -5,26 +5,35 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    //console.table(jsonObject);  // temporary checking for valid response and data parsing
     
     const towns = jsonObject['towns'];
-    const filtertowns = towns.filter(towns => towns.name == 'Preston' || towns.name == 'Soda Springs' || towns.name == 'Fish Haven');
 
-    filtertowns.forEach(towns => {
+    for (let i = 0; i < towns.length; i++ ) {
+      if (towns[i].name == 'Soda Springs' || towns[i].name == 'Fish Haven' || towns[i].name == 'Preston');
       let card = document.createElement('section');
       let h2 = document.createElement('h2');
-      let h4 = document.createElement('h4')
+      let p = document.createElement('p')
+      let year = document.createElement('year')
+      let population = document.createElement('population')
+      let annual = document.createElement('annual')
       let img = document.createElement('img');
-
-      h2.textContent =  `${towns[i].name}`;
-      h4.textContent = 'Year Founded : '  + towns[i].yearFounded;
+    
+      h2.innerHTML =  `${towns[i].name}`;
+      p.innerHTML = `${towns[i].motto}`;
+      year.innerHTML = '<br> Year Founded: ' + `${towns[i].yearFounded}</br>`;
+      population.innerHTML = '<br> Population: ' + `${towns[i].currentPopulation}</br>`;
+      annual.innerHTML = '<br> Annual Raim Fall: ' + `${towns[i].averageRainfall}</br>`;
       img.setAttribute('src', towns[i].photo);
       img.setAttribute('alt', `Towns photos ${towns[i].name}!`);
 
       card.append(h2);
-      card.append(h4);
+      card.append(p);
+      card.append(year);
+      card.append(population);
+      card.append(annual);
       card.append(img);
 
-      document.querySelector('div.text-town').append(card);
-    })
-  });
+      document.querySelector('div.cards').append();
+    }}
+  );
