d31f964e6c9dcdbc1f313cd5ae41146a66a9ca6b
NRodriguess <nickrodrigues006@gmail.com>
2021-07-01 23:21:53 -0300

    lesson11

--- WDD230/lesson10/js/fc.js
+++ WDD230/lesson10/js/fc.js
@@ -6,32 +6,32 @@ const apiURL = `http://api.openweathermap.org/data/2.5/forecast?id=${cityid}&app
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
-        const fiveDay = jsObject.list.filter( forecast => forecast.dt_txt.includes('18:00:00'));
+        const forecastDay = jsObject.list.filter( forecast => forecast.dt_txt.includes('18:00:00'));
         const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-        console.log(fiveDay);
+        console.log(forecastDay);
 
-        fiveDay.forEach( x => {
-            const d = new Date(x.dt_txt);
-            const weekDay = days[d.getDay()];
+        forecastDay.forEach( x => {
+            const dt = new Date(x.dt_txt);
+            const weekDay = days[dt.getDay()];
             const dayHigh = Math.round(x.main.temp_max);
             const imagesrc = 'https://openweathermap.org/img/wn/' + x.weather[0].icon + '.png';
 
             let dayContainer = document.createElement('div');
-            let weekDayContainer = document.createElement('p');
+            let weekDayBox = document.createElement('p');
             let forecastImg = document.createElement('img');
             let dayHighTemp = document.createElement('p');
 
-            weekDayContainer.textContent = weekDay;
+            weekDayBox.textContent = weekDay;
             forecastImg.setAttribute('src', imagesrc);
             forecastImg.setAttribute('alt', "Weather Icon");
             dayHighTemp.textContent = dayHigh + ' °F';
 
             dayContainer.classList.add("five-day");
-            dayContainer.appendChild(weekDayContainer);
+            dayContainer.appendChild(weekDayBox);
             dayContainer.appendChild(forecastImg);
             dayContainer.appendChild(dayHighTemp);
 
-            document.querySelector('div.five-wrap').appendChild(dayContainer);
+            document.querySelector('div.five-forecast').appendChild(dayContainer);
 
             console.log(forecastImg)
 
